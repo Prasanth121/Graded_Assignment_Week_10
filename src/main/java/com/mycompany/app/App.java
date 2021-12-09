80255124b469c48ecd847840e77cf86144f5ab95
PRASANTH E <prasanth.e@hcl.com>
2021-12-09 21:49:31 +0530

    Graded Assignment Week 10

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Graded Assignment" );
+        System.out.pringln();
+        System.out.println( "HELLO DEV-OPS" );
+        System.out.println();
+        
+    }
+}
